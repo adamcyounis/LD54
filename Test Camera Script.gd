22f6922bb89b5e344fcd6c406b91949630d55b3b
Adam Younis <adam@upponhill.com>
2023-09-29 16:05:22 +1000

    Adds Demo Scene

---
+++ Test Camera Script.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var time = OS.get_ticks_msec()/1000.0
+	position.x = sin(time) * 10
