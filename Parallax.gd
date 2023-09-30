e1c438dea07a1ee77b9c4149b2db462658a69f84
Adam Younis <adam@upponhill.com>
2023-09-30 15:26:02 +1000

    Adds solution for procedural colliders on sprites

--- Parallax.gd
+++ Parallax.gd
@@ -24,12 +24,16 @@ func _process(_delta):
 	if(Engine.is_editor_hint()):
 		if recalculateGeometry:
 			
-			var body = find_children("*","CharacterBody2D", true)[0]
-			var sprite = find_children("*","Sprite2D", true)[0]
-			print("recalculating geometry")
-			print(sprite)
-			print(body)
+
+			var children : Array[Node] = find_children("*","Sprite2D", true)
+			var bodies : Array[Node] = find_children("*","StaticBody2D", true)
 			
+			if children.size() > 0 and bodies.size() > 0:
+				var body = find_children("*","StaticBody2D", true)[0]
+				var sprite = find_children("*","Sprite2D", true)[0]
+				print("recalculating geometry")
+				print(sprite)
+				create_polygon2d_from_sprite2d(sprite, body)			
 			recalculateGeometry = false
 		return
 	else:
@@ -41,4 +45,25 @@ func _process(_delta):
 func camPos():
 	return get_viewport().get_camera_2d().position
 
+
+func create_polygon2d_from_sprite2d(_sprite: Sprite2D, _body: StaticBody2D, ):
+	# Destroy Existing Collision Polygons
+	for node in find_children("*", "CollisionPolygon2D"):
+		node.queue_free()
+	
+	# Generate Bitmap from Sprite2D
+	var sprite = _sprite
+	var image = sprite.texture.get_image()
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(image)
+	
+	# Create Collision Polygon from Opaque Pixels
+	var polys = bitmap.opaque_to_polygons(Rect2(Vector2.ZERO, image.get_size()), 0.0)
+	for poly in polys:
+		var collision_polygon = CollisionPolygon2D.new()
+		collision_polygon.polygon = poly
+		_body.add_child(collision_polygon)
+		collision_polygon.set_owner(get_tree().get_edited_scene_root())
+		#get_tree().get_edited_scene_root()
+		collision_polygon.position -= sprite.texture.get_size() / 2
 
