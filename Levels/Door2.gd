dd2393a65d3281f0f0192c321843935af9a98f43
Adam Younis <adam@upponhill.com>
2023-10-02 05:32:30 +1100

    Adds second type of door to avoid circular references

---
+++ Levels/Door2.gd
@@ -0,0 +1,18 @@
+class_name Door2 extends Area2D
+
+@export_file("*.tscn") var path 
+@onready var next_scene = load(path)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	body_entered.connect(_on_body_entered)
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_body_entered(body):
+	#check to see if the body belongs to GameManager.singleton.player
+	if body == GameManager.singleton.player:
+		GameManager.singleton.prepare_level(next_scene)
